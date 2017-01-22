04ebe9bc7f468112a03e4a68c3f0db28f7f212ae
Gaston Sanchez <gaston.stat@gmail.com>
2017-01-22 12:50:06 -0800

    apps: add ch03-histograms

---
+++ apps/ch03-histograms/README.md
@@ -0,0 +1,32 @@
+# Histograms for NBA players data
+
+This is a Shiny app that generates histograms using data of NBA players from the season 2015-2016..
+
+
+## Motivation
+
+The goal is to provide examples of histograms and distributions of quantitative variables.  __Statistics, Chapter 3: The Histogram__ (pages 32-56):
+
+- A _variable_ is a characteristic of the subjects in a study. It can be either qualitative or quantitative.
+- A _histogram_ is a visual display used to look at the distribution of a quantitative variable.
+- A _histogram_ represents precents by area. It consists of a set of blocks. The area of each block represents the percentage of cases in the correspoding class interval.
+- With the _density scale_, the height of each block equals the percentage of cases in the the corresponding class interval, divided by the length of that interval.
+
+Reference: "Statistics" by David Freedman, Robert Pisani and Roger Purves (2007). Fourth Edition. Norton & Company.
+
+
+## Data
+
+The data set is in the `nba_players.csv` file (see `data/` folder) which contains 101 rows and 8 columns, although the app des not uses variables: `Name` and `Brand`
+
+
+## How to run it?
+
+
+```R
+library(shiny)
+
+# Easiest way is to use runGitHub
+runGitHub("introstat-spring-2017", "ucb-introstat", subdir = "apps/ch03-histograms")
+```
+
---
+++ apps/ch03-histograms/app.R
@@ -0,0 +1,73 @@
+# Title: Histograms with NBA Players Data
+# Description: this app uses data of NBA players to show various histograms
+# Author: Gaston Sanchez
+
+library(shiny)
+
+# data set
+nba <- read.csv('../../data/nba_players.csv', header = TRUE)
+
+# quantitative variables
+quantitative <- c(
+  "height","weight","salary","experience","age","games","games_started",
+  "minutes_played","field_goals","field_goal_attempts","field_goal_percent",
+  "points3","points3_attempts","points3_percent","points2","points2_attempts",
+  "points2_percent","effective_field_goal_percent","free_throws",
+  "free_throw_attempts","free_throw_percent","offensive_rebounds",
+  "defensive_rebounds","total_rebounds","assists","steals","blocks",
+  "turnovers","fouls","points")
+
+# select just quantitative variables
+dat <- nba[ ,quantitative]
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("NBA Players"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         selectInput("variable", "Select a Variable", 
+                    choices = colnames(dat), selected = 'height'),
+        
+         sliderInput("bins",
+                     "Number of bins:",
+                     min = 1,
+                     max = 50,
+                     value = 10),
+         
+         checkboxInput('density', label = strong('Density scale'))
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("histogram")
+      )
+   )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$histogram <- renderPlot({
+      # generate bins based on input$bins from ui.R
+      x    <- na.omit(dat[ ,input$variable])
+      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+      histogram <- hist(x, breaks = bins, 
+           probability = input$density,
+           col = 'gray80', border = 'white', las = 1, axes = FALSE,
+           main = paste("Histogram of", input$variable))
+      axis(side = 2, las = 1)
+      axis(side = 1, at = bins, labels = round(bins, 2))
+      
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
