33b28b59426e29b628f3b42f33cc6716066797ca
Gaston Sanchez <gaston.stat@gmail.com>
2017-02-15 10:10:34 -0800

    apps: update description in regression strips

--- apps/ch11-regression-strips/app.R
+++ apps/ch11-regression-strips/app.R
@@ -1,11 +1,6 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Title: Spread within vertical strips in regression
+# Description: this app uses Pearson's height data 
+# Author: Gaston Sanchez
 
 library(shiny)
 source('helpers.R')
