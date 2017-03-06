4c7954db71b5498b5a655486b62d2cce632674f5
Gaston Sanchez <gaston.stat@gmail.com>
2017-03-06 10:28:40 -0800

    apps: reduce steps of number of tosses, ch16-chance-error

---
+++ apps/ch16-chance-error/README.md
@@ -0,0 +1,33 @@
+# Chance Error
+
+This is a Shiny app that illustrates the concept of chance error when simulating tossing a coin a given number of times.
+
+
+## Motivation
+
+The goal is to provide a visual display motivated by John Kerrich's coin-tossing experiment __Statistics, Chapter 16: The Law of Averages__
+
+Reference: "Statistics" by David Freedman, Robert Pisani and Roger Purves (2007). Fourth Edition. Norton & Company.
+
+
+## Data
+
+The data simulates tossing a coin using the random binomial generator function `rbinom()`. The input parameters are the number of tosses, and optionally, the probability of heads.
+
+
+## Plot
+
+There are two options for the displayed plot: 
+
+1. shows the chance error (i.e. number of heads minus half the number of tosses) on the y-axis, and the number of tosses on the x-axis.
+2. shows the percent error (i.e. proportion of heads) on the y-axis, and the number of tosses on the x-axis.
+
+
+## How to run it?
+
+```R
+library(shiny)
+
+# Easiest way is to use runGitHub
+runGitHub("introstat-spring-2017", "ucb-introstat", subdir = "ch16-chance-error")
+```
---
+++ apps/ch16-chance-error/app.R
@@ -0,0 +1,102 @@
+# Title: Chance Error and Percent Error
+# Description: Chance error when tossing a coin (based on John Kerrich's)
+# Chapter 16: The Law of Averages, p 275-278
+# Author: Gaston Sanchez
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Give the page a title
+  titlePanel("Coin Tossing Experiment"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      numericInput("seed", label = "Random Seed:", 12345, 
+                   min = 10000, max = 50000, step = 1),
+      sliderInput("tosses", label = "Number of tosses:", 
+                  min = 100, max = 10000, value = 3000, step = 50),
+      radioButtons("error", label = "Display",
+                   choices = list("Chance error" = 1, 
+                                  "Percent error" = 2), 
+                   selected = 1),
+      hr(),
+      helpText('Total number of heads:'),
+      verbatimTextOutput("num_heads"),
+      helpText('Proportion of heads:'),
+      verbatimTextOutput("prop_heads")
+    ),
+    
+    # Create a spot for the barplot
+    mainPanel(
+      plotOutput("chancePlot")  
+    )
+  )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  seed <- reactive({
+    input$seed
+  })
+  tosses <- reactive({
+    input$tosses
+  })
+  
+  # Proportion of heads
+  output$num_heads <- renderPrint({ 
+    set.seed(seed())
+    flips <- rbinom(n = tosses(), 1, prob = 0.5)
+    sum(flips)
+  })
+  
+  # Proportion of heads
+  output$prop_heads <- renderPrint({ 
+    set.seed(seed())
+    flips <- rbinom(n = tosses(), 1, prob = 0.5)
+    round(100 * sum(flips) / tosses(), 2)
+  })
+  
+  # Fill in the spot we created for a plot
+  output$chancePlot <- renderPlot({
+    set.seed(input$seed)
+    tosses <- input$tosses
+    flips <- rbinom(n = tosses, 1, prob = 0.5)
+    num_heads <- cumsum(flips)
+    prop_heads <- (num_heads / 1:tosses)
+    num_tosses <- 1:tosses
+
+    # Render a barplot
+    difference <- num_heads[num_tosses] - (num_tosses / 2)
+    proportion <- prop_heads[num_tosses]
+    if (input$error == 1) {
+      plot(num_tosses, difference, 
+           col = '#627fe2', type = 'l', lwd = 2,
+           xlab = "Number of tosses",
+           ylab = '# of heads - 1/2 # of tosses',
+           axes = FALSE, main = 'Chance Error:  # successes - # expected')
+      abline(h = 0, col = '#88888855', lwd = 2, lty = 2)
+      axis(side = 2, las = 1)
+    } else {
+      plot(num_tosses, proportion, ylim = c(0, 1),
+           col = '#627fe2', type = 'l', lwd = 2,
+           xlab = 'Number of tosses',
+           ylab = 'Proportion of heads',
+           axes = FALSE, main = 'Percent Error:  % successes - % expected')
+      abline(h = 0.5, col = '#88888855', lwd = 2, lty = 2)
+      axis(side = 2, las = 1, at = seq(0, 1, 0.1))
+    }
+    axis(side = 1)
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
