3f8861d94aa340231dcdab848ccef1855798f597
Gaston Sanchez <gaston.stat@gmail.com>
2017-03-22 11:20:51 -0700

    apps: add ch20-sampling-men example

---
+++ apps/ch20-sampling-men/README.md
@@ -0,0 +1,38 @@
+# Sampling Men
+
+This is a Shiny app that illustrates the concept of chance errors in sampling.
+
+
+## Motivation
+
+The goal is to provide a visual display for the Introduction example in 
+__Statistics, Chapter 20: Chance Errors in Sampling__
+
+Reference: "Statistics" by David Freedman, Robert Pisani and Roger Purves (2007). Fourth Edition. Norton & Company.
+
+
+## Data
+
+The data consists of a box model with 6672 tickets: 3091 __1's__, and 3581 __0's__. 
+The 1's tickets represent men, while the 0's represent women.
+The app simulates taking samples from the box. There are two parameters, one is the sample size, and the other is the number samples (i.e. # of repetitions).
+
+
+## Plots
+
+There are two plots: 
+
+1. The first tab shows a histogram with the number of men in the samples.
+2. The second tab shows a histogram with the percentage of men in the samples.
+
+
+## How to run it?
+
+There are many ways to download the app and run it:
+
+```R
+library(shiny)
+
+# Easiest way is to use runGitHub
+runGitHub("introstat-spring-2017", "ucb-introstat", subdir = "ch20-sampling-men")
+```
---
+++ apps/ch20-sampling-men/app.R
@@ -0,0 +1,101 @@
+# Title: Roll dice and add spots
+# Description: Empirical vs Probability Histograms
+# Chapter 18: Probability Histograms, page 311
+# Author: Gaston Sanchez
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Give the page a title
+  titlePanel("Sampling Men (p 359)"),
+  
+  # Generate a row with a sidebar
+  sidebarLayout(      
+    
+    # Define the sidebar with one input
+    sidebarPanel(
+      fluidRow(
+        column(5, 
+               numericInput("tickets1", "men [1]", 3091,
+                            min = 1, max = 35, step = 1)),
+        column(5,
+               numericInput("tickets0", "women [0]", 3581,
+                            min = 1, max = 35, step = 1))
+      ),
+      numericInput("size", label = "Sample Size (# draws):", value = 100,
+                   min = 10, max = 1500, step = 1),
+      sliderInput("reps", label = "Number of samples (reps):", 
+                  min = 50, max = 2000, value = 100, step = 50),
+      numericInput("seed", label = "Random Seed:", 12345, 
+                   min = 10000, max = 50000, step = 1),
+      hr(),
+      helpText('Number average'),
+      verbatimTextOutput("num_avg"),
+      helpText('Percent average'),
+      verbatimTextOutput("perc_avg")
+    ),
+    
+    # Create tabs for plots
+    mainPanel(
+      tabsetPanel(type = "tabs",
+                  tabPanel("Number", plotOutput("numberPlot")),
+                  tabPanel("Percentage", plotOutput("percentPlot"))
+      )
+    )
+  )
+)
+
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  num_men <- reactive({
+    tickets <- rep(c(1, 0), c(input$tickets1, input$tickets0))
+    
+    set.seed(input$seed)
+    size <- input$size
+    samples <- 1:input$reps
+    for (i in 1:input$reps) {
+      samples[i] <- sum(sample(tickets, size = size))
+    }
+    samples
+  })
+  
+  # Number of men
+  output$num_avg <- renderPrint({ 
+    round(mean(num_men()), 2)
+  })
+  
+  # Percentage of men
+  output$perc_avg <- renderPrint({ 
+    round(100 * mean(num_men() / input$size), 2)
+  })
+  
+  # Plot with number of men in samples
+  output$numberPlot <- renderPlot({
+    # Render a barplot
+    barplot(table(num_men()), 
+            space = 0, las = 1,
+            xlab = 'Number of men',
+            ylab = '',
+            main = 'Sample Men')
+  })
+  
+  # Plot with percentage of men in samples
+  output$percentPlot <- renderPlot({
+    # Render a barplot
+    percentage_men <- round(100 * num_men() / input$size)
+    barplot(table(percentage_men) / length(num_men()), 
+            space = 0, las = 1,
+            xlab = 'Percentage of men',
+            ylab = 'Proportion',
+            main = 'Sample Men')
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
